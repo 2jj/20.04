9e2702979868f204de65cbb3b18547202b01b357
2jj <58620248+2jj@users.noreply.github.com>
2021-02-07 06:33:23 +0100

    Update as-root.sh

--- as-root.sh
+++ as-root.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
+#  curl https://raw.githubusercontent.com/2jj/ubuntu/main/2004.sh | (export LOGIN=? PASS=?; bash -)
 
+echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+echo "AuthorizedKeysFile /root/.ssh/authorized_keys" >> /etc/ssh/sshd_config
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+chmod o+rx /root /root/.ssh /root/.ssh/authorized_keys
+service sshd restart
+
+echo 'Unattended-Upgrade::Remove-Unused-Kernel-Packages "true";' >> /etc/apt/apt.conf.d/50unattended-upgrades
+echo 'Unattended-Upgrade::Remove-New-Unused-Dependencies "true";' >> /etc/apt/apt.conf.d/50unattended-upgrades-
+echo 'Unattended-Upgrade::Remove-Unused-Dependencies "true";' >> /etc/apt/apt.conf.d/50unattended-upgrades
+service unattended-upgrades restart
+
+useradd -ms /bin/bash -G sudo -p $(openssl passwd -1 $PASS) $LOGIN
+
+do-release-upgrade
+apt update -y
+apt install snapd
+
+snap install nvim --edge --classic
+snap install docker --edge
+snap install ripgrep --edge --classic
+
+su - $LOGIN
