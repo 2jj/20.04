b7a387af0f0836321e07588b1a73a0bd0633c017
2jj <58620248+2jj@users.noreply.github.com>
2021-02-07 07:57:30 +0100

    Update as-root.sh

--- as-root.sh
+++ as-root.sh
@@ -26,6 +26,8 @@ snap install ripgrep --edge --classic
 
 su - $LOGIN
 
+set -x
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ]P && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
