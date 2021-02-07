b51b739822cccebf20dc192deeb92e7769063b15
2jj <58620248+2jj@users.noreply.github.com>
2021-02-07 07:29:04 +0100

    Update as-root.sh

--- as-root.sh
+++ as-root.sh
@@ -14,7 +14,7 @@ service unattended-upgrades restart
 useradd -ms /bin/bash -G sudo -p $(openssl passwd -1 $PASS) $LOGIN
 
 apt -y update
-apt -y full-upgrade
+apt -y upgrade
 apt install snapd
 
 snap install nvim --edge --classic
