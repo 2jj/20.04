909f99d47c08f6d59b6c2ccfb1aecb18bb74e205
2jj <58620248+2jj@users.noreply.github.com>
2021-02-07 08:17:23 +0100

    Update as-root.sh

--- as-root.sh
+++ as-root.sh
@@ -29,8 +29,8 @@ su - $LOGIN
 set -x
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ]P && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install node
 
 mkdir $HOME/.config
