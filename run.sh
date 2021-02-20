645df46a23d4e5fba8eb89c8626a04e5e922dc22
2jj <58620248+2jj@users.noreply.github.com>
2021-02-20 08:11:13 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -42,11 +42,16 @@ snap install nvim --edge --classic
 # Switch to non-root user
 su - $L
 
+# node
 echo "+ adding nvm and latest node..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install node
+
+# npm-based apps
+npm install -g fd-find
+npm install -g nodemon
 
 # Log commands
 set -x
